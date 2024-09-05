c61c95ba0180a557c413cca1f6b6c33687f50711
NaveenEerla <157868422+NaveenEerla@users.noreply.github.com>
2024-09-05 11:30:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+
+    public class Hammer
+    {
+        public void Use() {
+            Console.WriteLine(  "Using the Hammer");
+        }
+
+    }
+    public class Sow
+    {
+        public void Use()
+        {
+            Console.WriteLine("Using Sow");
+        }
+    }
+
+    // Constructor Dependency Injection(DI)
+    public class Builder
+    {
+        private Hammer _hammer;
+        private Sow _sow;
+
+        public Builder(Hammer hammer, Sow sow)
+        {
+            _hammer = hammer;
+            _sow = sow;
+        }
+        public void BuildHose()
+        {
+            _hammer.Use();
+            _sow.Use();
+            Console.WriteLine(  "House Built");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hammer hammer = new Hammer();
+            Sow sow = new Sow();   
+            Builder builder = new Builder(hammer,sow);
+            builder.BuildHose();
+            Console.Read();
+        }
+    }
+}
